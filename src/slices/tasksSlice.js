fa0e1d4068a0f16fd6db968954f6223d01708917
niru9711 <nirusanant97@gmail.com>
2024-04-12 01:18:27 +0530

    comment-unnecessary-reducers

--- src/slices/tasksSlice.js
+++ src/slices/tasksSlice.js
@@ -86,20 +86,20 @@ const tasksSlice = createSlice({
     name: 'tasksSlice',
     initialState,
     reducers: {
-        addTaskToList:(state, action) =>{
-            const id = Math.round(Math.random() * 100)
-            console.log(id);
-            let task = {...action.payload, id};
-            state.tasksList.push(task);
-        },
+        // addTaskToList:(state, action) =>{
+        //     const id = Math.round(Math.random() * 100)
+        //     console.log(id);
+        //     let task = {...action.payload, id};
+        //     state.tasksList.push(task);
+        // },
 
         removeTaskFromList:(state,action) => {
             state.tasksList = state.tasksList.filter((task) => task.id !== action.payload.id)
         },
 
-        updateTaskInList:(state,action) => {
-            state.tasksList = state.tasksList.map((task) => task.id === action.payload.id ? action.payload : task)
-        },
+        // updateTaskInList:(state,action) => {
+        //     state.tasksList = state.tasksList.map((task) => task.id === action.payload.id ? action.payload : task)
+        // },
 
         setSelectedTask:(state,action) => {
             state.selectedTask = action.payload
